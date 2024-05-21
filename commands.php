2bd30f614573f1ce8b0576e71c33e551df1b6cfd
KASNIAC <kasniac10@gmail.com>
2024-05-21 14:57:21 -0600

    Adding linux compatibility

--- commands.php
+++ commands.php
@@ -1,14 +1,13 @@
 <?php
-   // For windows:
-   define('OS', 'WINDOWS');
-   define('MKDIR', "mkdir"); 
-   define('COPY', 'copy');
-   define('CMP', 'fc');
-
-   // For linux:
-   // define('OS', 'LINUX');
-   // define('MKDIR', "mkdir"); 
-   // define('CP', 'copy');
-   // define('CMP', 'diff');
-
+   if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') { // Windows
+      $OS = "WINDOWS";
+      $MKDIR = "mkdir";
+      $COPY = "copy";
+      $CMP = "fc";
+   } else { // Linux
+      $OS = "LINUX";
+      $MKDIR = "mkdir";
+      $COPY = "cp";
+      $CMP = "diff";
+   }
 ?>
--- create.php
+++ create.php
@@ -9,35 +9,29 @@ $division = $argv[1];
 $contest = $argv[2];
 $limit = $argv[3];
 
-shell_exec(MKDIR." $division");
-if (file_exists("$division")) {
-   shell_exec(MKDIR." $division\\$contest");
-   if (file_exists("$division\\$contest")) {
-      shell_exec(COPY." compila.php $division\\$contest"); // I don't check, its absence is not that serious
-      shell_exec(COPY." commands.php $division\\$contest"); // I don't check, its absence is not that serious
+shell_exec("$MKDIR $division");
+if (file_exists($division)) {
+   shell_exec("$MKDIR \"$division/$contest\"");
+   if (file_exists("$division/$contest")) {
+      shell_exec("$COPY compile.php \"$division/$contest\""); // I don't check, its absence is not that serious
       
       for ($c = 'A'; $c <= $limit; ++$c) {
-         shell_exec(MKDIR." $division\\$contest\\$c");
-         if (file_exists("$division\\$contest\\$c")) {
-            shell_exec(COPY." template.cpp $division\\$contest\\$c\\$c.cpp");
-
-            if (file_exists("$division\\$contest\\$c\\$c.cpp")) {
-               if(OS == "WINDOWS") {
-                  shell_exec("type NUL > $division\\$contest\\$c\\01.in");
-                  shell_exec("type NUL > $division\\$contest\\$c\\01.out");
-               } else if(OS == "LINUX"){
-                  shell_exec("type > $division\\$contest\\$c\\01.in");
-                  shell_exec("type > $division\\$contest\\$c\\01.out");
-               }
+         shell_exec("$MKDIR \"$division/$contest/$c\"");
+         if (file_exists("$division/$contest/$c")) {
+            shell_exec("$COPY template.cpp \"$division/$contest/$c/$c.cpp\"");
+            
+            if (file_exists("$division/$contest/$c/$c.cpp")) {
+               shell_exec("$COPY 01.in \"$division/$contest/$c/01.in\""); // I don't check, its absence is not that serious
+               shell_exec("$COPY 01.out \"$division/$contest/$c/01.out\""); // I don't check, its absence is not that serious
             } else {
-               die("Could not create file $c.cpp");
+               die("Could not copy file $c.cpp");
             }
          } else {
             die("Error creating directory $c");
          }
       }
    } else {
-      die("Error creating directory $contest into $division direcotry");
+      die("Error creating directory $contest into $division directory");
    }
 } else {
    die("Error creating directory $division");
