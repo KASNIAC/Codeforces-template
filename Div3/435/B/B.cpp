fbb6db4813d4cdbbd5168ac841116e130be3ce65
KASNIAC <kasniac10@gmail.com>
2024-05-20 23:21:42 -0600

    adding features

---
+++ Div3/435/B/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void ny(bool flag) { 
+   std::cout << (flag ? "YES\n" : "NO\n");
+}
+
+void solve() {
+
+}
+
+int main() {
+   std::cin.tie(nullptr);
+   std::ios_base::sync_with_stdio(false);
+
+   int t;
+   std::cin >> t;
+
+   while (t--) {
+      solve();
+   }
+}
---
+++ Div3/435/commands.php
@@ -0,0 +1,14 @@
+<?php
+   // For windows:
+   define('OS', 'WINDOWS');
+   define('MKDIR', "mkdir"); 
+   define('COPY', 'copy');
+   define('CMP', 'fc');
+
+   // For linux:
+   // define('OS', 'LINUX');
+   // define('MKDIR', "mkdir"); 
+   // define('CP', 'copy');
+   // define('CMP', 'diff');
+
+?>
